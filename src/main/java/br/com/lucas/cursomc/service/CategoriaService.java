3671cc25a9b9d28c0bd60461d42ef859e0d32af6
Lucas Fonseca <lucas.b4rn@gmail.com>
2019-01-09 17:35:56 -0200

    Ajustem no end point /categorias/{id}

--- src/main/java/br/com/lucas/cursomc/service/CategoriaService.java
+++ src/main/java/br/com/lucas/cursomc/service/CategoriaService.java
@@ -2,6 +2,7 @@ package br.com.lucas.cursomc.service;
 
 import java.util.Optional;
 
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +18,8 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(), null));
 
 	}
 
