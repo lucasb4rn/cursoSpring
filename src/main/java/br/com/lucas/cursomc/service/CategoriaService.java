63fcc8bd9b11363dafc3f0a3156d7f19028caf6e
Lucasb4rngit config --global user.email Lucas.b4rn@gmail.comgit config --global user.name Lucasb4rn <Lucas.b4rn@gmail.com>
2019-01-13 23:02:49 -0200

    Inserindo uma nova Categoria com Post

--- src/main/java/br/com/lucas/cursomc/service/CategoriaService.java
+++ src/main/java/br/com/lucas/cursomc/service/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import br.com.lucas.cursomc.domain.Categoria;
 import br.com.lucas.cursomc.repository.CategoriaRepository;
 import br.com.lucas.cursomc.service.exception.ObjectNotFoundException;
@@ -17,9 +18,15 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 
 		Optional<Categoria> obj = repo.findById(id);
-		
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+
+	}
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
 	}
 
 }
