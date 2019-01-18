c8959e4f2ee276c3df021f52fa7840eaa81c30b2
Lucas Fonseca <lucas.b4rn@gmail.com>
2019-01-18 17:50:03 -0200

    criação do seach e endpoint /produtos

---
+++ src/main/java/br/com/lucas/cursomc/service/ProdutoService.java
@@ -0,0 +1,43 @@
+package br.com.lucas.cursomc.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import br.com.lucas.cursomc.domain.Categoria;
+import br.com.lucas.cursomc.domain.Produto;
+import br.com.lucas.cursomc.repository.CategoriaRepository;
+import br.com.lucas.cursomc.repository.ProdutoRepository;
+import br.com.lucas.cursomc.service.exception.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository repo;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	public Produto find(Integer id) {
+
+		Optional<Produto> obj = repo.findById(id);
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+
+	}
+
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repo.search(nome, categorias, pageRequest);
+		
+	}
+
+}
