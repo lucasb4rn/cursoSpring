49f263737262e140e64c581cda2a6295985019e4
Lucas Fonseca <lucas.b4rn@gmail.com>
2019-01-25 18:09:38 -0200

    Implementando JWT

---
+++ src/main/java/br/com/lucas/cursomc/dto/CredenciaisDTO.java
@@ -0,0 +1,35 @@
+package br.com.lucas.cursomc.dto;
+
+import java.io.Serializable;
+
+public class CredenciaisDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String email;
+	private String senha;
+
+	public CredenciaisDTO() {
+	}
+
+	public CredenciaisDTO(String email, String senha) {
+		this.email = email;
+		this.senha = senha;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+}
